2a47e2ad34cfca4bbda82e0bb7941f258a862c5a
piratebrook <779389629@qq.com>
2024-01-16 11:29:47 +0800

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IMAGE_TAG="pixgen-frontend:latest"
+CONTAINER_NAME="pixgen-frontend"
+
+sudo docker stop $CONTAINER_NAME
+sudo docker rm -f $CONTAINER_NAME
+
+sudo docker build -t $IMAGE_TAG .
+sudo docker run -itd --name $CONTAINER_NAME -p 3000:3000 $IMAGE_TAG
