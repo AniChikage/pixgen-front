280da6d6b10a6bd1628d2fbd03f938498b3bf074
anichikage <877229225@qq.com>
2024-01-18 12:06:53 +0800

    aaa

--- components/home/IntroductionHome.tsx
+++ components/home/IntroductionHome.tsx
@@ -5,7 +5,8 @@ export default function Introdcution() {
         <div className="relative overflow-hidden bg-[url('https://preline.co/assets/svg/examples/polygon-bg-element.svg')] ">
         <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-16">
             <div className="max-w-2xl text-center mx-auto mt-10">
-                <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl light:text-white"><span className="text-blue-600">几秒内</span>创造令人惊叹的视觉效果 </h1>
+            <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl light:text-white"><span className="text-blue-600">几秒内</span>创造令人惊叹的视觉效果 </h1>
+            <h1 className="block text-3xl font-bold text-gray-800 sm:text-4xl md:text-5xl light:text-white">(维护中，请稍后重试) </h1>
                 {/* <p className="mt-3 text-lg text-gray-800 light:text-gray-400">AI + 快 + 智能</p> */}
             </div>
 
