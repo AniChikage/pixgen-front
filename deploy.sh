6770676babd10c696fb99e048358ebed36c48388
anichikage <877229225@qq.com>
2024-01-19 08:09:05 +0800

    add remove docker images

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ CONTAINER_NAME="pixgen-frontend"
 sudo docker stop $CONTAINER_NAME
 sudo docker rm -f $CONTAINER_NAME
 
+sudo docker rmi $IMAGE_TAG
 sudo docker build -t $IMAGE_TAG .
 sudo docker run -itd --name $CONTAINER_NAME -p 3000:3000 $IMAGE_TAG
